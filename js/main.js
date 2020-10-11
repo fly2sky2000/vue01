39343814007444c8ac47d27cddcd2c0f5fe2cd97
fly2sky2000 <fly2sky2000@sina.com.cn>
2020-10-12 00:04:26 +0800

    提交一下

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 var vm = new Vue({
-    el: "#app",
-    data: {
-      attr1: 1,
-      joke: "笑话",
+  el: "#app",
+  data: {
+    attr1: 1,
+    joke: "笑话",
+  },
+  methods: {
+    getJoke: function () {
+      console.log(this.attr1);
+      var that = this;
+      ("http://wthrcdn.etouch.cn/weather_mini");
+      axios.get("https://autumnfish.cn/api/joke/list?num=3").then(
+        function (response) {
+          console.log(response);
+          that.joke = response.data.jokes[0];
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
     },
-    methods: {
-      getJoke: function () {
-        console.log(this.attr1);
-        var that = this;
-        "http://wthrcdn.etouch.cn/weather_mini";
-        axios.get("https://autumnfish.cn/api/joke/list?num=3").then(
-          function (response) {
-            console.log(response);
-            that.joke=response.data.jokes[0];
-          },
-          function (err) {
-            console.log(err);
-          }
-        );
-      },
-    },
-  });
+  },
+});
